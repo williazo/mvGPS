a6809b3a73e421ae706478ee10fff33c494e0fb4
Justin Williams <williazo@ucla.edu>
2020-07-16 15:21:00 -0700

    full test coverage of mvGPS()

--- tests/testthat/test-mvGPS.R
+++ tests/testthat/test-mvGPS.R
@@ -5,6 +5,10 @@ C1 <- matrix(unlist(lapply(seq_len(3), function(m) rnorm(100))), nrow=100)
 C2 <- matrix(unlist(lapply(seq_len(2), function(m) rnorm(100))), nrow=100)
 C3 <- matrix(unlist(lapply(seq_len(1), function(m) rnorm(100))), nrow=100)
 C <- list(C1, C2, C3)
+
+dt <- data.frame(D, C1, C2, C3)
+names(dt) <- paste0("X", seq_len(ncol(dt)))
+
 test_that("Argument formatting", {
     expect_error(mvGPS(D[, 1], C), "Exposure must be multivariate. See details to ensure formula is properly specified")
     
@@ -30,6 +34,10 @@ test_that("Expected output", {
     expect_equal(length(out), 2)
     expect_named(out, c("score", "wts"))
     expect_equal(unname(lapply(out, length)), rep(list(100), 2))
+    
+    # checking that you can specify data.frames as well as matrices
+    out_dt <- mvGPS(dt[, 1:3], C=list(dt[, 4:6], dt[, 7:8], dt[, 9]), common=FALSE)
+    expect_equal(out$wts, out_dt$wts)
     
     out <- mvGPS(D, C1, common=TRUE)
     expect_equal(length(out), 2)
